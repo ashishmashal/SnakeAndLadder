f9b2cbeba7d137ef18b62268a32dd7f45dca8bfa
ashishmashal <ashishmashal5@gmail.com>
2022-04-24 22:53:15 +0530

    Added: Snake And Ladder PLayers Rolling Dies Added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,9 +1,16 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static final int START_POSITION_LADDER = 0;
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Game");
+        System.out.println("Roll The Dies");
+        Random random = new Random();
          int position= 0;
+         int dies = random.nextInt(6) + 1;
+        System.out.println("Dies Value Is " + dies);
+
     }
 }
