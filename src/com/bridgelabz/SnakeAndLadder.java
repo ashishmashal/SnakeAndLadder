0aafea960a41d2c92ff18fb8ee2b89352a2c9c54
ashishmashal <ashishmashal5@gmail.com>
2022-04-24 23:03:00 +0530

    Added:Snake and Ladder Movement Of The Player Logic Option Is Added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,6 +4,9 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     public static final int START_POSITION_LADDER = 0;
+    public static final int NO_FUTHER_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Game");
         System.out.println("Roll The Dies");
@@ -12,5 +15,19 @@ public class SnakeAndLadder {
          int dies = random.nextInt(6) + 1;
         System.out.println("Dies Value Is " + dies);
 
+        int options = random.nextInt(3);
+        System.out.println(options);
+        switch (options){
+            case LADDER :
+                position += dies;
+                break;
+            case SNAKE:
+                if (position != 0)
+                    position -= dies;
+                break;
+            case NO_FUTHER_PLAY:
+                break;
+        }
+        System.out.println("Player At Position " + position);
     }
 }
