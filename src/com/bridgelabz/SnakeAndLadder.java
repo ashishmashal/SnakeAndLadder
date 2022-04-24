bc4c89fa4e6d122587c74fa282411cca2ac7cb24
ashishmashal <ashishmashal5@gmail.com>
2022-04-24 22:45:21 +0530

    Added: Snake And Ladder Starting Position Added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,7 +1,9 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+    public static final int START_POSITION_LADDER = 0;
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Game");
+         int position= 0;
     }
 }
