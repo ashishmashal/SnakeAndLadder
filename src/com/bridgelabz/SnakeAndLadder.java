30749e7b9a34ec0460878cecf91f38018ee70a1c
ashishmashal <ashishmashal5@gmail.com>
2022-04-25 19:50:27 +0530

    Added: Repeat Till 100

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,33 +1,48 @@
 package com.bridgelabz;
-
 import java.util.Random;
 
 public class SnakeAndLadder {
     public static final int START_POSITION_LADDER = 0;
     public static final int NO_FUTHER_PLAY = 0;
+    public static final int END_POSITION_LADDER = 100;
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
-    public static void main(String[] args) {
-        System.out.println("Welcome To Snake And Ladder Game");
-        System.out.println("Roll The Dies");
-        Random random = new Random();
-         int position= 0;
-         int dies = random.nextInt(6) + 1;
-        System.out.println("Dies Value Is " + dies);
+    static int currentPosition;
+    static String position;
+    static Random random = new Random();
 
-        int options = random.nextInt(3);
-        System.out.println(options);
-        switch (options){
-            case LADDER :
-                position += dies;
+    public static int dieRoll() {
+        int diceValue = random.nextInt(6) + 1;
+        System.out.println("Dice value : " + diceValue);
+        return diceValue;
+    }
+    public static int playerChecksOption() {
+        int currentPosition = START_POSITION_LADDER;
+        int checkOption = random.nextInt(3);
+        int dice = dieRoll();
+        switch (checkOption) {
+            case NO_FUTHER_PLAY:
+                currentPosition = currentPosition;
+                position = "Not played";
+                break;
+            case LADDER:
+                currentPosition += dice;
+                position = "Player is on Ladder";
                 break;
             case SNAKE:
-                if (position != 0)
-                    position -= dies;
-                break;
-            case NO_FUTHER_PLAY:
+                if (currentPosition <START_POSITION_LADDER)
+                currentPosition -= dice;
+                position = "Player is on Snake";
                 break;
         }
-        System.out.println("Player At Position " + position);
+        System.out.println("Player's Position :" + position + ",  #Current Position :" + currentPosition);
+        return currentPosition;
     }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game");
+        playerChecksOption();
+
+    }
+
 }
